2d4ed4013e1b8ab18d2f172fce8bac1b18b66609
tumasas20 <tumasas20@gmail.com>
2023-03-01 11:33:26 +0200

    feat(): cao lesson 3 exercise 1

---
+++ cao-homework/lesson 3/pratimas 1/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const brands = ['Audi', 'BMV', 'MB', 'VW', 'Opel'];
+
+app.get("/brands", (_req, res) => {
+    res.send(brands);
+});
+
+app.post("/brands", (req, res) => {
+    console.log(req.body);
+    res.send('Created brand');
+});
+
+app.listen(8080, () => console.log("The service is runing on port http://localhost:8080"));
