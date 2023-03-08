2642e835ff9d4d7725ef7fa460d05af9ae4fb13a
tumasas20 <tumasas20@gmail.com>
2023-03-08 11:59:44 +0200

    feat(): created sql migration table structure

--- node-js/src/services/database-services.ts
+++ node-js/src/services/database-services.ts
@@ -4,11 +4,12 @@ import config from 'config';
 const connect = (callback: VoidFunction) => {
     const connetion = mysql.createConnection(config.database);
 
-    connetion.query('SELECT 1', (err) => {
+    connetion.query('SELECT 1 + 1', (err, a) => {
       if (err !== null) {
         console.error('ERROR: Connecting to database was unsuccessful');
         throw err;
       }
+      console.log(a);
 
       callback();
       connetion.end();
---
+++ node-js/src/sql/migrations/migration-2023-03-07-15-36-table-structure.sql
@@ -0,0 +1,49 @@
+create table image (
+    imageId int4 unsigned primary key auto_increment,
+    src varchar(512) not null,
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp
+);
+
+create table user (
+    userId int4 unsigned primary key auto_increment,
+    name varchar(64) not null,
+    surname varchar(64) not null,
+    email varchar(64) not null unique,
+    password varchar(64) not null,
+    imageId int4 unsigned not null unique,
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp,
+    foreign key (imageId) references image(imageId)
+);
+
+create table film (
+    filmId int4 unsigned primary key auto_increment,
+    title varchar(64) not null,
+    year varchar(4) not null,
+    userId int4 unsigned not null,
+    createdAt timestamp default current_timestamp,
+    updatedAt timestamp default current_timestamp on update current_timestamp,
+    foreign key (userId) references user(userId)
+);
+
+create table actor (
+	actorId int4 unsigned primary key auto_increment,
+	fullname varchar(128) not null unique,
+	createdAt timestamp default current_timestamp,
+	updatedAt timestamp default current_timestamp on update current_timestamp
+);
+
+create table role (
+	actorId int4 unsigned not null,
+	filmId int4 unsigned not null,
+    foreign key (actorId) references actor(actorId),
+    foreign key (filmId) references film(filmId)
+);
+
+create table film_image (
+	filmId int4 unsigned not null,
+	imageId int4 unsigned not null primary key,
+    foreign key (filmId) references film(filmId),
+    foreign key (imageId) references image(imageId)
+);
