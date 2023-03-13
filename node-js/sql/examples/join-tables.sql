3fb41ddb26146bcb9041d6c2ca8af89710a33387
tumasas20 <tumasas20@gmail.com>
2023-03-13 21:16:05 +0200

    feat(): joined tables in sql

---
+++ node-js/sql/examples/join-tables.sql
@@ -0,0 +1,32 @@
+select 
+	f.filmId as id,
+	f.title,
+	f.year,
+    json_object(
+		'id', u.userId,
+		'name', u.name,
+		'surname', u.surname,
+		'email', u.email
+	)  as host, 
+	json_object(
+        'fullname', ac.fullname
+	) as actors,
+	json_arrayagg(i.src) as images
+from film as f
+
+join user as u
+on f.userId = u.userId
+
+join role as r
+on f.filmId = r.actorId
+
+join actor as ac
+on r.actorId = ac.actorId 
+
+join film_image as fi
+on f.filmId = fi.filmId
+
+join image as i
+on fi.imageId = i.imageId
+
+group by f.filmId
